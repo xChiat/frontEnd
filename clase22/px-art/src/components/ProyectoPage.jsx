1145f263c990f4698b10f83525da85a877c0841b
cristian <chiat.contact@gmail.com>
2024-07-08 11:55:40 -0400

    ya todo funciona :D

--- clase22/px-art/src/components/ProyectoPage.jsx
+++ clase22/px-art/src/components/ProyectoPage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import Header from './Header';
 
 function ProyectoPage() {
     const [proyecto, setProyecto] = useState(null);
     const [paletas, setPaletas] = useState([]);
     const [colorSeleccionado, setColorSeleccionado] = useState('');
     const [paletaSeleccionada, setPaletaSeleccionada] = useState(null);
+    const [escenaActual, setEscenaActual] = useState(null);
 
     useEffect(() => {
         try {
@@ -13,6 +15,7 @@ function ProyectoPage() {
             if (proyectoEnEdicion) {
                 const parsedProyecto = JSON.parse(proyectoEnEdicion);
                 setProyecto(parsedProyecto);
+                setEscenaActual(parsedProyecto._escena); // Escena inicial por defecto
                 setPaletaSeleccionada(parsedProyecto._escena._paletas[0]); // Paleta inicial por defecto
                 setColorSeleccionado(parsedProyecto._escena._paletas[0].colores[0]);
             } else {
@@ -29,9 +32,9 @@ function ProyectoPage() {
         }
     }, []);
 
-    if (!proyecto) return <div>Loading...</div>;
+    if (!proyecto || !escenaActual) return <div>Loading...</div>;
 
-    const dimensiones = proyecto._escena._pixeles;
+    const dimensiones = escenaActual._pixeles;
 
     const handleColorClick = (color) => {
         setColorSeleccionado(color);
@@ -44,6 +47,12 @@ function ProyectoPage() {
     const handlePaletaClick = (paleta) => {
         setPaletaSeleccionada(paleta);
         setColorSeleccionado(paleta.colores[0]); // Cambiar al primer color de la paleta seleccionada
+    };
+
+    const handleEscenaClick = (escena) => {
+        setEscenaActual(escena);
+        setPaletaSeleccionada(escena._paletas[0]);
+        setColorSeleccionado(escena._paletas[0].colores[0]);
     };
 
     const renderGrid = (dimensiones) => {
@@ -60,21 +69,24 @@ function ProyectoPage() {
 
     return (
         <div className="container-xxl text-center">
+            <div className='row'>
+                <Header/>
+            </div>
             <div className="row">
                 <div className="col-2">
-                    <div className='result'>
+                    <div className='result' onClick={() => handleEscenaClick(proyecto._escena)} style={{ cursor: 'pointer' }}>
                         <h5>Escenas</h5>
-                        <p>{proyecto._escena._nombre} - {dimensiones} x {dimensiones} pixels</p>
+                        <p>{proyecto._escena._nombre} - {proyecto._escena._pixeles} x {proyecto._escena._pixeles} pixels</p>
                     </div>
                     {proyecto._escenas.slice(1).map((escena, index) => (
-                        <div key={index} className='result'>
+                        <div key={index} className='result' onClick={() => handleEscenaClick(escena)} style={{ cursor: 'pointer' }}>
                             <p>{escena._nombre} - {escena._pixeles} x {escena._pixeles} pixels</p>
                         </div>
                     ))}
                 </div>
                 <div className="col-8">
                     <div className='cuadricula'>
-                        <h5>{proyecto._escena._nombre}</h5>
+                        <h5>{escenaActual._nombre}</h5>
                         <div className="grid-container">
                             {renderGrid(dimensiones)}
                         </div>
